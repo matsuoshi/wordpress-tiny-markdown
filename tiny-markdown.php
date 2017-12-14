7be8d75eea51297d6290deab6e785c86f31424b6
matsuoshi <matsuoshi@gmail.com>
2017-12-14 18:43:56 +0900

    urlリンク処理の修正

--- tiny-markdown.php
+++ tiny-markdown.php
@@ -23,29 +23,26 @@ class TinyMarkdown
 
         // markdown 初期化
         $this->pd = new Parsedown();
-        $this->pd->setBreaksEnabled(true); // 改行だけで改行する
+        // 改行だけで改行する + URLをリンクしない
+        $this->pd->setBreaksEnabled(true)->setUrlsLinked(false);
 
         // markdown呼び出しフィルタ設定
         add_filter('the_content', array(&$this, 'parsedown'));
         add_filter('comment_text', array(&$this, 'parsedown'));
-
-        // 管理画面の投稿時、テキストエディタしか使えなくする
-        add_filter('wp_default_editor', function () {
-                return 'html';
-            });
-
-        add_action('admin_print_styles', function () {
-                echo <<< _STYLE_
-<style>
-    #ed_toolbar input { visibility: hidden; }
-</style>
-_STYLE_;
-            });
     }
 
     public function parsedown($content)
     {
-        return '<div class="markdown">' . $this->pd->text($content) . '</div>';
+        // embed時の崩れ対応
+        $content = str_replace('</blockquote><iframe', "</blockquote>\n<iframe", $content);
+
+        // parse
+        $content = '<div class="markdown">' . $this->pd->text($content) . '</div>';
+
+        // url link
+        $content = preg_replace('#(?<!href="|">)(https?://[^\s<]+)\b#i', '<a href="$1">$1</a>', $content);
+
+        return $content;
     }
 }
 
